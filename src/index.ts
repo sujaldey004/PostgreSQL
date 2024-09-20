644b7c7cfebabba0e2c853d0670140f5d349bb56
sujaldey004 <sujaldey2920@gmail.com>
2024-09-20 11:40:22 +0530

    Create Users Table

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ async function createUsersTable(){
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAP
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
         );
     `)
     console.log(result);
